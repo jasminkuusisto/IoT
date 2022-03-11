713812f227a19e5f763ac802143b4b8b2a27489e
Tuukka <tuukka.petteri@gmail.com>
2022-03-11 19:25:15 +0200

    Add table for better structure

--- iot-alarm-app/src/App.js
+++ iot-alarm-app/src/App.js
@@ -1,37 +1,11 @@
-import Header from './components/Header'
-import Record from './components/Alarm_record'
+import Header from './components/Header';
+import RecTable from './components/Alarm_table';
 
 function App() {
-  //const records = useState(data)[0];
-  const records = [
-    {
-      "message": 'This is how alarms will be shown', 
-      "time": '18:50'
-    },
-    {
-      "message": 'Example alarm 1', 
-      "time": '19:00'
-    }
-  ];
-
   return (
     <div className="container">
       <Header />
-
-      <table className="alarm-record-table">
-        <thead>
-          <tr>
-            <th>Info</th>
-            <th>Time</th>
-          </tr>
-        </thead>
-        <tbody>
-          {records.map((rec, i) => (
-            <Record key={i} rec={rec}/>
-          ))}
-        </tbody>
-      </table>
-
+      <RecTable />
     </div>
   );
 }
---
+++ iot-alarm-app/src/components/Alarm_table.js
@@ -0,0 +1,34 @@
+import Record from './Alarm_record'
+
+const RecTable = () => {
+    // Get data from SQL database
+    //const records = useState(data)[0];
+    const records = [
+        {
+        "message": 'This is how alarms will be shown', 
+        "time": '19:00'
+        },
+        {
+        "message": 'Example alarm 1', 
+        "time": '18:50'
+        }
+    ];
+
+    return ( 
+        <table className="alarm-record-table">
+        <thead>
+          <tr>
+            <th>Info</th>
+            <th>Time</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((rec, i) => (
+            <Record key={i} rec={rec}/>
+          ))}
+        </tbody>
+      </table>
+    );
+}
+ 
+export default RecTable;
