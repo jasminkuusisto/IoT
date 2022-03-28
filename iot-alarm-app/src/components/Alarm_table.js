5c7fccfd05c7fcbb0b09bd3cf75caf88bf4fac4c
Pauliina <pauliina.hippula@tuni.fi>
2022-03-28 13:32:07 +0300

    Added the fetching clause for fetching the data from the database

--- iot-alarm-app/src/components/Alarm_table.js
+++ iot-alarm-app/src/components/Alarm_table.js
@@ -1,18 +1,23 @@
 import Record from './Alarm_record'
 
 const RecTable = () => {
+    let records = {}
+    fetch('http://localhost:3001/records/')
+      .then(response => response.json())
+      .then(data => records = data);
+
     // Get data from SQL database
     //const records = useState(data)[0];
-    const records = [
-        {
-        "message": 'This is how alarms will be shown', 
-        "time": '19:00'
-        },
-        {
-        "message": 'Example alarm 1', 
-        "time": '18:50'
-        }
-    ];
+    //const records = [
+        //{
+        //"message": 'This is how alarms will be shown', 
+        //"time": '19:00'
+        //},
+        //{
+        //"message": 'Example alarm 1', 
+        //"time": '18:50'
+        //}
+    //];
 
     return ( 
         <table className="alarm-record-table">
