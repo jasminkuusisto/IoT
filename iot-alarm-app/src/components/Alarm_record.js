61ac337ba6ebeec8be7d2c34c8cd911f592e5163
Tuukka <tuukka.petteri@gmail.com>
2022-04-05 10:10:06 +0300

    Fetch records every 10 seconds

--- iot-alarm-app/src/components/Alarm_record.js
+++ iot-alarm-app/src/components/Alarm_record.js
@@ -1,12 +1,10 @@
 const Record = ({ rec }) => {
-    let time = rec.date.split('T')[0];
-    const [year, month, day] = time.split('-');
-    time = `${day}/${month}/${year}`;
+    let d = new Date(Date.parse(rec.date))
 
     return (
         <tr>
             <td className="record">{rec.description}</td>
-            <td className="record">{time}</td>
+            <td className="record">{d.toString().split('G')[0]}</td>
         </tr>
     );
 }
--- iot-alarm-app/src/components/Alarm_table.js
+++ iot-alarm-app/src/components/Alarm_table.js
@@ -5,29 +5,50 @@ const RecTable = () => {
   const [records, setRecords] = useState([])
 
   useEffect (() => {
-    fetch('http://localhost:3001/records/', {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    })
-      .then(res => res.json())
-      .then(data => setRecords(data))
+    (function fetchData() {
+      console.log('interval fired')
+      fetch('http://localhost:3001/records/', {
+        headers: {
+          'Access-Control-Allow-Origin': '*'
+        }
+      })
+        .then(res => res.json())
+        .then(data => setRecords(data.reverse()))
+
+      setTimeout(fetchData, 10000);
+    })();
+
   }, []);
 
+  let lastRecordToday = false;
+  if (records.length > 0) {
+    const lastRecordDt = new Date(Date.parse(records[0].date));
+    const now = new Date();
+    const msBetweenDates = Math.abs(lastRecordDt.getTime() - now.getTime());
+    const hoursBetweenDates = msBetweenDates / (60 * 60 * 1000);
+
+    if (hoursBetweenDates < 24) {
+      lastRecordToday = true;
+    }
+  }
+
   return (
-    <table className="alarm-record-table">
-      <thead>
-        <tr>
-          <th>Record Info</th>
-          <th>Date</th>
-        </tr>
-      </thead>
-      <tbody>
-        {records.map((rec, i) => (
-          <Record key={i} rec={rec}/>
-        ))}
-      </tbody>
-    </table>
+    <div>
+      {lastRecordToday ? <p>An alarm was fired within 24 hours!</p> : ''}
+      <table className="alarm-record-table">
+        <thead>
+          <tr>
+            <th>Description</th>
+            <th>Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          {records.map((rec, i) => (
+            <Record key={i} rec={rec}/>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
  
